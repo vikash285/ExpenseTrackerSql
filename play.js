d26a252ad2d536c9afa6d14c8790c21f53fa7b4a
vikash285 <vikashdhaka285@gmail.com>
2023-02-04 22:45:24 +0530

    first req, res task

--- play.js
+++ play.js
@@ -1,13 +1,26 @@
 const http=require('http')
 const server=http.createServer((req,res)=>{
-    console.log(req.url, req.method, req.headers);
-    // process.exit()
-    res.setHeader('Content-Type','text/html')
+    if(req.url==='/home'){
     res.write('<html>')
     res.write('<head><title>My First Page</title></head>')
-    res.write('<body><h1>Hello from my Node.js server!</h1><body>')
+    res.write('<body><h1>Welcome Home!</h1><body>')
     res.write('</html>')
     res.end()
+    }
+    else if(req.url==='/about'){
+    res.write('<html>')
+    res.write('<head><title>My First Page</title></head>')
+    res.write('<body><h1>Welcome to about us page!</h1><body>')
+    res.write('</html>')
+    res.end()
+    }
+    else if(req.url==='/node'){
+    res.write('<html>')
+    res.write('<head><title>My First Page</title></head>')
+    res.write('<body><h1>Welcome to my Node.js project!</h1><body>')
+    res.write('</html>')
+    res.end()
+    }
 })
 
 server.listen(4000);
