8a8e8bcabe051c4d1bdc4805de9c0856827c1b3a
vikash285 <vikashdhaka285@gmail.com>
2023-02-05 15:03:55 +0530

    clean code

--- play.js
+++ play.js
@@ -1,43 +1,8 @@
 const http=require('http')
-const fs=require('fs')
-const server=http.createServer((req,res)=>{
-    const url=req.url
-    const method=req.method
-    if(url==='/'){
-        fs.readFile("message.txt",{encoding:"utf-8"},(err,data)=>{
-            if(err){
-                console.log(err);
-            }
-            res.write('<html>')
-            res.write('<head><title>Enter Message</title></head>')
-            res.write(`<body>${data}</body>`)
-            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
-            res.write('</html>')
-            return res.end()
-        })
-    }
-    else if(url==='/message' && method==='POST'){
-        const body=[]
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk)
-        })
-        return req.on('end',()=>{
-            const parsedBody=Buffer.concat(body).toString()
-            const message=parsedBody.split('=')[1]
-            fs.writeFile('message.txt',message,(err)=>{
-                res.statusCode=302
-                res.setHeader('Location','/')
-                return res.end()
-            })
-        })
-    }else{
-    res.write('<html>')
-    res.write('<head><title>My First Page</title></head>')
-    res.write('<body><h1>Welcome to about us page!</h1><body>')
-    res.write('</html>')
-    res.end()
-    }
-})
+const route=require('./routes')
+
+console.log(route.someText);
+
+const server=http.createServer(route.handler)
 
 server.listen(4000);
