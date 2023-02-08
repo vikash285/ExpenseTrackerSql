8089701b3005dfdb8788d1abd268f36602df6afe
vikash285 <vikashdhaka285@gmail.com>
2023-02-08 11:39:20 +0530

    changed routes

--- play.js
+++ play.js
@@ -3,19 +3,16 @@ const bodyParser=require('body-parser')
 
 const app=express()
 
-app.use(bodyParser.urlencoded({extended: false}))
+const adminRoutes=require('./routes/admin')
+const shopRoutes=require('./routes/shop')
 
-app.use('/add-product',(req, res, next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>')
-})
+app.use(bodyParser.urlencoded({extended:true}))
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/')
-})
+app.use('/admin',adminRoutes)
+app.use('/shop',shopRoutes)
 
-app.use('/',(req, res, next)=>{
-    res.send('<h1>Hello from Express!</h1>')
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found!</h1>')
 })
 
 app.listen(4000)
