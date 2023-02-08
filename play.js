1bca244a7be75947f1b68d1618466ab2bf29fbe1
vikash285 <vikashdhaka285@gmail.com>
2023-02-08 10:54:33 +0530

    body parser

--- play.js
+++ play.js
@@ -1,15 +1,21 @@
 const express=require('express')
+const bodyParser=require('body-parser')
 
 const app=express()
 
-app.use((req, res, next)=>{
-    console.log('In the middleware!');
-    next()
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/add-product',(req, res, next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>')
 })
 
-app.use((req, res, next)=>{
-    console.log('In another middleware!');
-    res.send({key1:'value'})
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/')
+})
+
+app.use('/',(req, res, next)=>{
+    res.send('<h1>Hello from Express!</h1>')
 })
 
 app.listen(4000)
