de19c0fffe355202aeb720579c82be26dc1f4d59
vikash285 <vikashdhaka285@gmail.com>
2023-02-09 15:11:50 +0530

    contact us into controllers

--- routes/inquiery.js
+++ routes/inquiery.js
@@ -1,18 +1,10 @@
 const path=require('path')
 const express=require('express')
-const rootDir=require('../util/path')
+const inquieryController=require('../controllers/inquiery')
 const router=express.Router()
 
-router.get('/contactUs',(req,res)=>{
-    res.send(`<form action="/success" method="POST">
-    <label for="name">Name:</label><br><input type="text" id="name" name="name"><br>
-    <label for="email">Email:</label><br><input type="email" id="email" name="email"><br>
-    <button type="submit">Add Details</button>
-    </form>`)
-})
+router.get('/contactUs',inquieryController.contactUs)
 
-router.post('/success',(req,res)=>{
-    res.send('<h1>Form successfully filled</h1>')
-})
+router.post('/success',inquieryController.postSuccess)
 
 module.exports=router
