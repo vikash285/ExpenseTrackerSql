9f2e1aa49bab9951c8dbe09d660803a6d41e33c2
vikash285 <vikashdhaka285@gmail.com>
2023-02-14 19:01:18 +0530

    booking appoinment app

--- play.js
+++ play.js
@@ -6,7 +6,12 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database')
 
+const User = require('./models/user')
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors())
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -14,11 +19,41 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.post('/user/add-user', async(req, res, next) => {
+   try {
+
+    if(!req.body.phonenumber) {
+        throw new Error('Phonenumber is mandatory!')
+    }
+    const name = req.body.name;
+    const email = req.body.email;
+    const phonenumber = req.body.phonenumber;
+
+    const data = await User.create({ name: name, email: email, phonenumber: phonenumber })
+    res.status(201).json({ newUserDetail: data })
+   } catch (err) {
+    res.status(500).json({
+        error: err
+    })
+   }
+})
+
+app.get('/user/get-users', async(req, res, next) => {
+    const users = await User.findAll()
+    res.status(200).json({ allUsers: users })
+})
+
+app.delete('/user/delete-user/:id', async(req, res, next) => {
+    const uId = req.params.id
+    await User.destroy({ where: { id: uId }})
+    res.sendStatus(200)
+})
 
 app.use(errorController.get404);
 
