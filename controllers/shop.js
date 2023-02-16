4fc23576de5a9e7d90899450158b61b201f5ec6e
vikash285 <vikashdhaka285@gmail.com>
2023-02-16 15:29:46 +0530

    adding and deleteing cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -39,18 +39,66 @@ exports.getIndex = (req, res, next) => {
   }
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
-  });
+  req.udemyUser.getCart()
+  .then(cart => {
+    return cart.getProducts()
+    .then(products => {
+        res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products,
+      })
+    })
+    .catch(err => console.log(err))
+  })
+  .catch(err => console.log(err))
 };
 
 exports.postCart = (req, res, next) => {
   const prodId=req.body.productId
-  Product.findById(prodId, product => {
-    Cart.addProduct(prodId, product.price)
+  let fetchedCart
+  let newQuantity = 1
+
+  req.udemyUser.getCart()
+  .then(cart => {
+    fetchedCart = cart
+    return cart.getProducts({ where: { id: prodId }})
   })
-  res.redirect('/cart')
+  .then(products => {
+    let product
+    if (products.length > 0) {
+      product = products[0]
+    }
+    if (product) {
+       const oldQuantity = product.cartItem.quantity
+       newQuantity = oldQuantity + 1
+       return product
+    }
+    return Product.findByPk(prodId)
+  })
+    .then(product => {
+      return fetchedCart.addProduct(product, { through: { quantity: newQuantity }})
+    })
+  .then(() => {
+    res.redirect('/cart')
+  })
+  .catch(err => console.log(err))
+}
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId
+  req.udemyUser.getCart()
+  .then(cart => {
+    return cart.getProducts({ where: { id: prodId }})
+  })
+  .then(products => {
+    const product = products[0]
+    return product.cartItem.destroy()
+  })
+  .then(result => {
+    res.redirect('/cart')
+  })
+  .catch(err => console.log(err))
 }
 
 exports.getOrders = (req, res, next) => {
