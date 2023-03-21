9e8a0e06e0a25df211553cb2a8e0d10666fbe1e3
vikash285 <vikashdhaka285@gmail.com>
2023-03-21 18:34:00 +0530

    silly route mistake

--- routes/userApp.js
+++ routes/userApp.js
@@ -3,8 +3,8 @@ const express = require('express')
 const userAppController = require('../controllers/userApp')
 const router = express.Router()
 
-router.post('/signUP', userAppController.postLogin)
+router.post('/signUP', userAppController.postSignUp)
 
-router.post('/login', userAppController.postSignUp)
+router.post('/login', userAppController.postLogin)
 
 module.exports = router
