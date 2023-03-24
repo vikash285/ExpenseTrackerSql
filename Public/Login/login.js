dd8448fbb046a1da24e05e9774358c4bb6627431
vikash285 <vikashdhaka285@gmail.com>
2023-03-24 14:25:19 +0530

    changing AWS ec2 user

--- Public/Login/login.js
+++ Public/Login/login.js
@@ -8,7 +8,7 @@ async function logIn(event) {
             password: event.target.password.value
         }
 
-        const res = await axios.post("http://54.197.207.33:3000/userApp/login", loginDetails)
+        const res = await axios.post("http://52.72.31.169:3000/userApp/login", loginDetails)
         
             alert(res.data.message)
             localStorage.setItem('token', res.data.token)
